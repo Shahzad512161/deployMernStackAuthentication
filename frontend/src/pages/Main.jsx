9c7f67a4e0b5af95e4270014fefdf1010663acc1
Muhammad Shahzad Diyal <147616022+Shahzad512161@users.noreply.github.com>
2025-01-27 13:54:16 +0500

    Update Main.jsx

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -22,7 +22,7 @@ const Main = () => {
 
     const fetchProducts = async () => {
         try {
-            const url = url('http://localhost:8080/product')
+            const url = url('https://deploy-mern-stack-authentication-irdgv2qlj.vercel.app/product')
             const headers = {
                 header:{
                     'Authorization':localStorage.getItem('token')
