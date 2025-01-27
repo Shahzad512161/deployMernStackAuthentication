be0d40d984e296595c8a0acb05ef330d1914b92f
Muhammad Shahzad Diyal <147616022+Shahzad512161@users.noreply.github.com>
2025-01-27 13:53:57 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     }
   
     try {
-      const response = await fetch('http://localhost:8080/auth/login', {
+      const response = await fetch('https://deploy-mern-stack-authentication-irdgv2qlj.vercel.app/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
